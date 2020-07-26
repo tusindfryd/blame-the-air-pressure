3d0944ca6cfaa4d08bbb428e2f75f459fdf62e92
tusindfryd <dominikagoral99@gmail.com>
2020-07-26 20:27:09 +0200

    initial commit :tada:

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Barometer } from 'expo-sensors';
 
 export default function App() {
+  const [pressure, setPressure] = useState(null)
+  const [caption, setCaption] = useState(null)
+  const captionsPositive = ["...yeah", "sure", "it seems so", "why not", "yes"]
+  const captionsNegative = ["not really", "who's gonna check anyway", "...no", "well..."]
+
+  useEffect(() => {
+    if (Barometer.isAvailableAsync()) {
+      Barometer.addListener(barometerData => {
+        setPressure(barometerData.pressure)
+      })
+      if (pressure != 1013) {
+        setCaption(captionsPositive[0])
+      } else {
+        setCaption(captionsNegative[0])
+      }
+    }
+  }, []);
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center"
+    },
+    header: {
+
+    },
+    pressureTextStyle: {
+
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      alignItems: 'stretch',
+      marginTop: 15,
+    },
+    button: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#eee',
+      padding: 10,
+    },
+    sensor: {
+      marginTop: 45,
+      paddingHorizontal: 10,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>
+        Can You Blame the Atmospheric Pressure?
+      </Text>
+      <Text style={styles.pressureTextStyle}>{pressure} hPa</Text>
+      <Text style={styles.captionTextStyle}>{caption}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
