2a189ec5489b79545ac2b373f3a851ee1ad2b2ad
tusindfryd <dominikagoral99@gmail.com>
2020-07-28 20:50:12 +0200

    no barometer? no problem!

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import { Barometer } from 'expo-sensors';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Location from 'expo-location';
+
+import { key } from './key.json'; // Open Weather Map API key
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [retries, setRetries] = useState(0);
+  const [locationPermissions, setLocationPermissions] = useState(null);
+  const [barometerAvailable, setBarometerAvailable] = useState(null);
   const [pressure, setPressure] = useState(null)
   const [caption, setCaption] = useState(null)
   const captionsPositive = ["...yeah", "sure", "it seems so", "why not", "yes"]
@@ -13,18 +21,55 @@ export default function App() {
     return (array[(Math.floor(Math.random() * max) + 1) % max])
   }
 
+  let callWeatherStation = () => {
+    let lat = location.coords.latitude.toString();
+    let lon = location.coords.longitude.toString();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+      .then(function (response) { return response.json(); })
+      .then(function (response) {
+        setPressure(response.main.pressure);
+        if (pressure != 1013) {
+          setCaption(chooseRandomWord(captionsPositive))
+        } else {
+          setCaption(chooseRandomWord(captionsNegative))
+        }
+        SplashScreen.hideAsync();
+      });
+  }
+
   useEffect(() => {
-    if (Barometer.isAvailableAsync()) {
-      Barometer.addListener(barometerData => {
-        setPressure(barometerData.pressure)
-      })
-      if (pressure != 1013) {
-        setCaption(chooseRandomWord(captionsPositive))
-      } else {
-        setCaption(chooseRandomWord(captionsNegative))
+    (async () => {
+      await SplashScreen.preventAutoHideAsync();
+      if (await Barometer.isAvailableAsync()) {
+        setBarometerAvailable(true);
+        Barometer.addListener(barometerData => {
+          setPressure(barometerData.pressure)
+        })
+        if (pressure != 1013) {
+          setCaption(chooseRandomWord(captionsPositive))
+        } else {
+          setCaption(chooseRandomWord(captionsNegative))
+        }
+        SplashScreen.hideAsync();
       }
-    }
-  }, []);
+      else {
+        setBarometerAvailable(false);
+        let { status } = await Location.requestPermissionsAsync();
+        if (status !== 'granted') {
+          setLocationPermissions(false);
+          SplashScreen.hideAsync();
+        } else {
+          setLocationPermissions(true);
+          setLocation(await Location.getCurrentPositionAsync({ accuracy: 6 }));
+          if (location == null) {
+            setRetries(retries + 1);
+          } else {
+            callWeatherStation();
+          }
+        }
+      }
+    })();
+  }, [retries]);
 
   const styles = StyleSheet.create({
     backgroundImage: {
@@ -47,6 +92,15 @@ export default function App() {
     }
   });
 
+  if (!barometerAvailable && !locationPermissions) {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./assets/splash.png')} style={styles.backgroundImage}></Image>
+        <Text style={styles.captionTextStyle}>Location permission not granted.</Text>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require('./assets/splash.png')} style={styles.backgroundImage}></Image>
@@ -55,4 +109,3 @@ export default function App() {
     </View>
   );
 }
-
